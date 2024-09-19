1c7005a20893afae2c2936d749db21596fbdf12b
farukjnu1@gmail.com <farukjnu1@gmail.com>
2024-09-19 11:59:57 +0600

    Add project files.

---
+++ TestFactoryMethodDesignPattern/NotificationSystem/NotificationCreator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestFactoryMethodDesignPattern.NotificationSystem
+{
+    public abstract class NotificationCreator
+    {
+        public abstract INotification CreateNotification();
+
+        public void Notify(string message)
+        {
+            INotification notification = CreateNotification();
+            notification.Send(message);
+        }
+    }
+}
---
+++ TestFactoryMethodDesignPattern/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestFactoryMethodDesignPattern.NotificationSystem;
+
+namespace TestFactoryMethodDesignPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NotificationCreator emailCreator = new EmailNotificationCreator();
+            emailCreator.Notify("Email message content.");
+
+            NotificationCreator smsCreator = new SmsNotificationCreator();
+            smsCreator.Notify("SMS message content.");
+
+            NotificationCreator pushCreator = new PushNotificationCreator();
+            pushCreator.Notify("Push message content.");
+        }
+    }
+}
